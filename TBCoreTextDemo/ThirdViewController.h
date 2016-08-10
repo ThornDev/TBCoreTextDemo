954d63deaadad8730175de5b12396b1dc12e809e
algolstep <jiemianwang@gmail.com>
2016-08-10 18:06:12 +0800

    iMac

---
+++ TBCoreTextDemo/ThirdViewController.h
@@ -0,0 +1,14 @@
+//
+//  ThirdViewController.h
+//  TBCoreTextDemo
+//
+//  Created by 思来氏 on 16/8/8.
+//  Copyright © 2016年 ThornDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIWebView *tWebView;
+@end
